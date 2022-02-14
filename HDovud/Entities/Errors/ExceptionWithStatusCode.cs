771f2418ec6589e197f7bfb58217170a4233db7b
Payrav-1997 <62432924+Payrav-1997@users.noreply.github.com>
2022-02-14 23:25:51 +0500

    Finished Middleware

---
+++ HDovud/Entities/Errors/ExceptionWithStatusCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HDovud.Entities.Errors
+{
+    public class ExceptionWithStatusCode : Exception
+    {
+        public int StatusCode { get; set; }
+    }
+}
---
+++ HDovud/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using HDovud.Entities.Errors;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace HDovud.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionMessageAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionMessageAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+
+            context.Response.StatusCode = ((ExceptionWithStatusCode)ex).StatusCode;
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(new ErrorDetails
+            {
+                Message = ex.InnerException?.Message ?? ex.Message
+            }));
+        }
+    }
+}
